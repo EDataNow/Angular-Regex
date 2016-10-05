7baeea0e421489b75d71c9ec4fb5dc94cccbc8b0
naemcivic <naemcivic@gmail.com>
2016-10-05 15:42:26 -0400

    add materilize 2 to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import "materialize-css";
+import { MaterializeDirective } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterializeDirective
   ],
   imports: [
     BrowserModule,
