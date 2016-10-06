934f0e7e30e55af11b06aabc82d081e9edaa6dd0
naemcivic <naemcivic@gmail.com>
2016-10-06 16:18:41 -0400

    write functions and tests for different cases of RegEX requirements

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,30 +4,93 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('App: AngularRegex', () => {
+  var app: AppComponent
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
-  });
+    app = new AppComponent();
+  })
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  it('should match on first character', () => {
+    let barcode1 = 'P12345-A500-H1';
+    let barcode2 = '123TYU890';
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+    expect(app.matchOnFirstCharacter(barcode1, 'P')).toEqual('P12345-A500-H1');
+    expect(app.matchOnFirstCharacter(barcode2, 'P')).not.toEqual('P12345-A500-H1');
+  });
 
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+  it('should match on whole string', () => {
+    let barcode1 = 'P12345-A500-H1';
+
+    expect(app.matchOnAnyPosition(barcode1, 'A5')).toEqual('P12345-A500-H1');
+  });
+
+    it('should match on first character, return string proceeding match; including match', () => {
+    let barcode1 = 'P12345-A500-H1';
+
+    expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode1, 'A5')).toEqual('A500-H1');
+    expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode1, 'a5')).not.toEqual('A500-H1');
+  });
+
+  it('should match on first character, return string proceeding match; not including match', () => {
+    let barcode1 = 'P12345-A500-H1';
+
+    expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode1, 'A5')).toEqual('00-H1');
+    expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode1, 'X5')).not.toEqual('00-H1');
+  });
+
+   it('should match any position from given string and specific, including match', () => {
+    let barcode1 = 'P12345-A500-H1';
+    let result = app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode1, 'A5', 2)
+
+    expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode1, 'A5', 2)).toEqual('A500');
+    expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode1, 'X5', 2)).not.toEqual('A500');
+  });
+
+  it('should match on first character, return string proceeding match; not including match', () => {
+    let barcode1 = 'P12345-A500-H1';
+    let result = app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode1, 'A5', 2)
+
+    expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode1, '0', 3)).toEqual('0-H');
+    expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode1, '1', 6)).toEqual('2345-A');
+    expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode1, 'X5', 2)).not.toEqual('00');
+  });
+
+ it('should match after given index, including match', () => {
+    let barcode1 = 'P12345-A500-H1';
+
+    expect(app.matchGivenIndexIncludeMatch(barcode1, 3)).toEqual('2345-A500-H1');
+    expect(app.matchGivenIndexIncludeMatch(barcode1, 5)).toEqual('45-A500-H1');
+    expect(app.matchGivenIndexIncludeMatch(barcode1, 9)).toEqual('500-H1');
+    expect(app.matchGivenIndexIncludeMatch(barcode1, 1)).toEqual('P12345-A500-H1');
+    expect(app.matchGivenIndexIncludeMatch(barcode1, 4)).not.toEqual('2345-A500-H1');
+  });
+
+it('should match after given index, not including match', () => {
+    let barcode1 = 'P12345-A500-H1';
+    let result = app.matchGivenIndexNotIncludeMatch(barcode1, 2)
+
+    expect(app.matchGivenIndexNotIncludeMatch(barcode1, 2)).toEqual('2345-A500-H1');
+    expect(app.matchGivenIndexNotIncludeMatch(barcode1, 4)).toEqual('45-A500-H1');
+    expect(app.matchGivenIndexNotIncludeMatch(barcode1, 3)).not.toEqual('2345-A500-H1');
+  });
+
+it('should match after given index and specific length, including match', () => {
+    let barcode1 = 'P12345-A500-H1';
+
+    expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode1, 8, 3)).toEqual('A500');
+    expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode1, 8, 5)).toEqual('A500-H'); 
+    expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode1, 2, 1000)).toEqual('12345-A500-H1');  
+    expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode1, 100, 1000)).toEqual('');  
+    expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode1, 5, 3)).not.toEqual('A500');
+  });
+
+it('should match after given index and specific length, not including match', () => {
+    let barcode1 = 'P12345-A500-H1';
+
+    expect(app.matchGivenIndexAndSpecificLengthNotIncludeMatch(barcode1, 8, 3)).toEqual('500');
+    expect(app.matchGivenIndexAndSpecificLengthNotIncludeMatch(barcode1, 5, 3)).not.toEqual('500');
+  });
+
+
+  
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,142 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  ngOnInit () {}
+
+// #1
+// Match string on any position, non-case sensitive. Returns the whole string  
+
+
+ matchOnAnyPosition(barcode, input_string){
+    let input = `${input_string}`
+    let regEx = new RegExp(input)
+    let barcodeMatch = regEx.test(barcode);
+  
+    if (barcodeMatch) {
+      return barcode
+    } else {
+      return "No Match"
+    }
+  }
+
+// Case #2
+// Case sensitive match on FIRST letter, returns the whole string
+
+  matchOnFirstCharacter(barcode, input_string){
+    let input = `^${input_string}`
+    let regEx = new RegExp(input)
+    let barcodeMatch = regEx.test(barcode);
+  
+    if (barcodeMatch) {
+      return barcode
+    } else {
+      return "No Match"
+    }
+  }
+
+// Case #3
+// Match on any postion, case sensitive. returns the string proceeding from 
+// match including the match
+
+ matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode, input_string){
+   let input = `${input_string}`
+   let regEx = new RegExp(input)
+   let barcodeMatch = regEx.exec(barcode);
+
+   if (barcodeMatch) {
+   let output = barcode.slice(barcodeMatch.index);
+     return output
+   } else {
+     return "No Match"
+   }
+  }
+
+// Case #4
+// Match on any position, case sensitive. returns the string proceeding from match NOT 
+// including the match
+
+ matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode, input_string){
+   let input = `${input_string}`
+   let regEx = new RegExp(input)
+   let barcodeMatch = regEx.exec(barcode);
+
+   if (barcodeMatch) {
+   let output = barcode.slice(barcodeMatch.index + input.length);
+     return output
+   } else {
+     return "No Match"
+   }
+  }
+
+// Case #5
+// Match on any position, case sensitive. returns the string proceeding from match 
+// including the match and specific length after the match
+
+ matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength (barcode, input_string, length){
+   let input = `${input_string}`;
+   let input_length = length
+   let regEx = new RegExp(input)
+   let match = regEx.exec(barcode);
+    
+   if (match) {
+    return barcode.slice(match.index, match.index + input_length + input.length);
+   } else {
+     return "No Match"
+   }
+  }
+
+// Case #6
+// Match on any position, case sensitive. returns the string proceeding from match 
+// not including the match and specific length after the match
+matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, input_string, length){
+   let input = `${input_string}`;
+   let input_length = length 
+   let regEx = new RegExp(input)
+   let match = regEx.exec(barcode);
+    
+   if (match) {
+    return barcode.slice(match.index + input.length, match.index + input.length + length);
+   } else {
+     return "No Match"
+   }
+  }
+
+// Case #7
+// Match on given index, include match.
+ matchGivenIndexIncludeMatch(barcode, start){
+   let start_index = start - 1
+   let result = barcode.slice(start_index)
+   return result;
+}
+
+// Case #8
+// Match on given index, not include match.
+ matchGivenIndexNotIncludeMatch(barcode, start){
+   let start_index = start
+   let result = barcode.slice(start_index)
+   return result;
+}
+
+// Case #9
+// Match on given index and given length, include match.
+ matchGivenIndexAndSpecificLengthIncludeMatch(barcode, start, length){
+   let start_index = start - 1
+   let input_length = length + 1
+   let result = barcode.slice(start_index, start_index + input_length);
+   return result;
+}
+
+// Case #10
+// Match on given index and given length, include match.
+ matchGivenIndexAndSpecificLengthNotIncludeMatch(barcode, start, length){
+   let start_index = start
+   let input_length = length
+   let result = barcode.slice(start_index, start_index + input_length);
+   return result;
+}
+
+
+  
+
 }
