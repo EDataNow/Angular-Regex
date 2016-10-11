581e409165864c095096e5f1026eaa183bc9b841
Saim Zahid <saimzahid@me.com>
2016-10-11 16:48:40 -0400

    Finished connecting UI to functions

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,84 +9,52 @@ describe('AppComponent: AngularRegex', () => {
     app = new AppComponent();
   })
 
-  describe('matching string on first character or from the whole string', () => {
-      it('should match whole string character', () => {
-        expect(app.matchOnAnyPosition(barcode, '1')).toEqual('12345-A500-H1');
-      });
-      
-      it('should match on first character', () => {
-        expect(app.matchOnFirstCharacter(barcode, 'P')).toEqual('P12345-A500-H1');
-      });
-      it('Should return null when no match is found', () => {
-        expect(app.matchOnAnyPosition(barcode, 'X')).toBe(null);
-        expect(app.matchOnFirstCharacter(barcode, '1')).toBe(null);
-      })
-    })
 
-//   it('should match whole string character', () => {
-// //     let barcode = 'P12345-A500-H1';
-//     expect(app.matchOnAnyPosition(barcode, '1')).toEqual('12345-A500-H1');
-//     expect(app.matchOnAnyPosition(barcode, 'X')).toBe(null);
-//   });
+  describe('Matching after given string', ()=> {
 
-//   it('should match on first character', () => {
+    it('#matchOnAnyPositionReturnStringProceedingMatchIncludingMatch should match after given string, including match', ()=> {
+      expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode, 'A5')).toEqual('A500-H1');
+    });
 
-//     expect(app.matchOnFirstCharacter(barcode, 'P')).toEqual('P12345-A500-H1');
-//   });
+    it('#matchOnAnyPositionReturnStringProceedingMatchIncludingMatch should only return matching string, if said string is at the end of barcode', ()=> {
+      expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode, 'H1')).toEqual('H1');
+    });
 
-//   it('should match on first character, return string proceeding match; including match', () => {
+    it('#matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch should match after given string, not including match', ()=> {
+      expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode, 'A')).toEqual('500-H1');
+    });
 
-//     expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode, 'A5')).toEqual('A500-H1');
-//     expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode, '1')).toEqual('12345-A500-H1');
-//   });
+    it('#matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch should return empty if matching string is at the end of the barcode', ()=> {
+      expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode, 'H1')).toEqual('');
+    });
 
-//   it('should match on first character, return string proceeding match; not including match', () => {
+    it('#matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength should return matching string of given length, including match', ()=> {
+      expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode, 'A5', 2)).toEqual('A500');
+    });
 
-//     expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode, 'A5')).toEqual('00-H1');
-//     expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode, 'H')).toEqual('1');
-//   });
+    it('#matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength should return matching string of given length, including match', ()=> {
+      expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, '0', 3)).toEqual('0-H');
+    });
+
+
+    it('should return null if given string is at the end of the barcode', ()=> {
+      expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode, 'H1', 2)).toEqual(null);
+      expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, 'H1', 2)).toEqual(null);
+    });
+
+
+  })
+
+
 
 //    it('should match any position from given string and specific, including match', () => {
-
-//     expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode, 'A5', 2)).toEqual('A500');
-//     expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode, '1', 3)).toEqual('1234');
-//   });
+//     expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode, 'A5', 2)).toEqual('A500');
+//     expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode, '1', 3)).toEqual('1234');
+//   });
 
 //   it('should match on first character, return string proceeding match; not including match', () => {
-
-//     expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, '0', 3)).toEqual('0-H');
+//     expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, '0', 3)).toEqual('0-H');
 //     expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, '1', 6)).toEqual('2345-A');
-//     expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, 'X5', 2)).toBe(null);
-//   });
-
-//  it('should match after given index, including match', () => {
-
-//     expect(app.matchGivenIndexIncludeMatch(barcode, 3)).toEqual('2345-A500-H1');
-//     expect(app.matchGivenIndexIncludeMatch(barcode, 5)).toEqual('45-A500-H1');
-//     expect(app.matchGivenIndexIncludeMatch(barcode, 9)).toEqual('500-H1');
-//     expect(app.matchGivenIndexIncludeMatch(barcode, 1)).toEqual('P12345-A500-H1');
-//     expect(app.matchGivenIndexIncludeMatch(barcode, 99)).toEqual(null);
-//   });
-
-// it('should match after given index, not including match', () => {
-
-//     expect(app.matchGivenIndexNotIncludeMatch(barcode, 2)).toEqual('2345-A500-H1');
-//     expect(app.matchGivenIndexNotIncludeMatch(barcode, 4)).toEqual('45-A500-H1');
-//     expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode, 8, 5)).toEqual('A500-H');
-//     expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode, 1, 5)).toEqual('P12345');    expect(app.matchGivenIndexIncludeMatch(barcode, 99)).toEqual(null);
-//   });
-
-//  it('should match after given index and specific length, including match', () => {
-
-//     expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode, 8, 3)).toEqual('A500');
-
-//     expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode, 100, 3)).toBe(null);
-//   });
-
-//  it('should match after given index and specific length, not including match', () => {
-
-//     expect(app.matchGivenIndexAndSpecificLengthNotIncludeMatch(barcode, 8, 3)).toEqual('500');
-//     expect(app.matchGivenIndexAndSpecificLengthNotIncludeMatch(barcode, 1000, 3)).toBe(null);
-//   });
+//     expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, 'X5', 2)).toBe(null);
+//   });
 });
-
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'app works!';
+includeMatch:boolean = true;
+match_on_dropdown: number;
+return_dropdown: number;
+specificLength:number = 0;
+startIndex:number = 0;
 
-  ngOnInit () {}
+  ngOnInit () { }
 
-regEx(input, barcode, index){
-  let regEx = new RegExp(input);
-  let match = regEx.exec(barcode);
+      regExFunctionFilter(barcode, input_string, match_on_dropdown, return_dropdown, includeMatch, specificLength, startIndex) {
+        const output = document.getElementById("output")
+       // Case 1
+        if (this.caseOne(match_on_dropdown, return_dropdown, includeMatch)) {
+            output.innerText = this.matchOnAnyPosition(barcode, input_string);
+        }
+        //Case 2
+        if (this.caseTwo(match_on_dropdown, return_dropdown, includeMatch)) {
+            output.innerText = this.matchOnFirstCharacter(barcode, input_string);
+        }
 
-   if (match) {
-    return match[index]
-   } else {
-     return null
-   }
-}
+        //Case 3 & 4
+        if (this.caseThreeAndFour(match_on_dropdown, return_dropdown, includeMatch)) {
+            if (includeMatch === "true"){
+                output.innerText = this.matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode, input_string);
+            } else {
+               output.innerText = this.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode, input_string);
+            }
+        }
+
+        //Case 5 & 6
+        if (this.caseFiveAndSix(match_on_dropdown, return_dropdown, includeMatch, specificLength)) {
+            if (includeMatch === "true"){
+                output.innerText = this.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength (barcode, input_string, specificLength);
+            } else {
+               output.innerText = this.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, input_string, specificLength);
+            }
+        }
+
+         //Case 7 & 8
+        if (this.caseSevenAndEight(match_on_dropdown, return_dropdown, includeMatch, startIndex)) {
+            if (includeMatch === "true"){
+                output.innerText = this.matchGivenIndexIncludeMatch(barcode, startIndex);
+            } else {
+                output.innerText = this.matchGivenIndexNotIncludeMatch(barcode, startIndex);
+            }
+        }
+
+        //Case 9 & 10
+        if (this.caseNineAndTen(match_on_dropdown, return_dropdown, includeMatch, startIndex, specificLength)) {
+            if (includeMatch === "true"){
+                output.innerText = this.matchGivenIndexAndSpecificLengthIncludeMatch(barcode, parseInt(startIndex), parseInt(specificLength));
+            } else {
+                output.innerText = this.matchGivenIndexAndSpecificLengthNotIncludeMatch(barcode, startIndex, specificLength)
+            }
+        }
+    }
+
+    private caseOne(match_on_dropdown, return_dropdown, includeMatch) {
+        if (match_on_dropdown === "2" && return_dropdown === "1" && includeMatch === "true" ) {
+            return true
+        }
+    }
+
+    private caseTwo(match_on_dropdown, return_dropdown, includeMatch) {
+        if (match_on_dropdown === "1" && return_dropdown === "1" && includeMatch === "true" ) {
+            return true
+        }
+    }
+
+    private caseThreeAndFour(match_on_dropdown, return_dropdown, includeMatch) {
+        if (match_on_dropdown === "2" && return_dropdown === "3" && (includeMatch === "true" || includeMatch === "false") ) {
+            return true
+        }
+    }
+
+    private caseFiveAndSix(match_on_dropdown, return_dropdown, includeMatch, specificLength ) {
+        if (match_on_dropdown === "2" && return_dropdown === "2" && (includeMatch === "true" || includeMatch === "false") && specificLength > 0 ) {
+            return true
+        }
+    }
+
+    private caseSevenAndEight(match_on_dropdown, return_dropdown, includeMatch, startIndex) {
+        if (match_on_dropdown === "3" && return_dropdown === "3" && (includeMatch === "true" || includeMatch === "false") && startIndex > 0 ) {
+            return true
+        }
+    }
+
+    private caseNineAndTen(match_on_dropdown, return_dropdown, includeMatch, startIndex, specificLength) {
+        if (match_on_dropdown === "3" && return_dropdown === "2" && (includeMatch === "true" || includeMatch === "false") && startIndex > 0  && specificLength > 0 ) {
+            return true
+        }
+    }
+
+
 
 // #1
 // Match string on any position, non-case sensitive. Returns the whole string
 
-
- matchOnAnyPosition(barcode, input_string){
-  let input = `${input_string}` + '(.+)';
-  let index = 0;
-  return this.regEx(input, barcode, index);
+ matchOnAnyPosition(barcode, input_string){
+  let input = '(^.*' + `${input_string}` + '.*$)';
+  let index = 0;
+  return this.regEx(input, barcode, index);
 }
 
 
@@ -37,30 +115,30 @@ regEx(input, barcode, index){
 // Case sensitive match on FIRST letter, returns the whole string
 
   matchOnFirstCharacter(barcode, input_string){
-    let input = '^' + `${input_string}` + '(.+)';
+    let input = '^' + `${input_string}` + '(.+)';
     let index = 0;
-    return this.regEx(input, barcode, index);
-  }
+    return this.regEx(input, barcode, index);
+  }
 
 // Case #3
 // Match on any postion, case sensitive. returns the string proceeding from
 // match including the match
 
  matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode, input_string){
-   let input = `${input_string}` + '(.*)'
+   let input = `${input_string}` + '(.*)'
    let index = 0;
-   return this.regEx(input, barcode, index);
-  }
+   return this.regEx(input, barcode, index);
+  }
 
 // Case #4
 // Match on any position, case sensitive. returns the string proceeding from match NOT
 // including the match
 
  matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode, input_string){
-    let input = `${input_string}` + '(.*)' //input(.*)
+    let input = `${input_string}` + '(.*)' //input(.*)
     let index = 1;
-    return this.regEx(input, barcode, index);
-  }
+    return this.regEx(input, barcode, index);
+  }
 
 
 // Case #5
@@ -68,129 +146,120 @@ regEx(input, barcode, index){
 // including the match and specific length after the match
 
  matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength (barcode, input_string, length){
-   let input = '(' + `${input_string}` + '.' + '{' + `${length}` +'})'; //(input.{length})
+   let input = '(' + `${input_string}` + '.' + '{' + `${length}` +'})'; //(input.{length})
    let index = 0;
-   return this.regEx(input, barcode, index);
-  }
+   return this.regEx(input, barcode, index);
+  }
 
 // Case #6
 // Match on any position, case sensitive. returns the string proceeding from match
 // not including the match and specific length after the match
 matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, input_string, length){
-   let input = `${input_string}`+ '(.' + '{' + `${length}` +'})'; //input(.{length})
+   let input = `${input_string}`+ '(.' + '{' + `${length}` +'})'; //input(.{length})
    let index = 1;
-   return this.regEx(input, barcode, index);
-  }
+   return this.regEx(input, barcode, index);
+  }
 
 // Case #7
-  // Match on given index, include match.
-  matchGivenIndexIncludeMatch(barcode, start){
-    let input = `^.{${start - 1}}(.+)` //`^.{start - 1}(.+)`
-    let index = 1;
-    return this.regEx(input, barcode, index); 
-  }
+  // Match on given index, include match.
+  matchGivenIndexIncludeMatch(barcode, start){
+    let input = `^.{${start - 1}}(.+)` //`^.{start - 1}(.+)`
+    let index = 1;
+    return this.regEx(input, barcode, index);
+  }
 
-  // Case #8
-  // Match on given index, exclude match.
-  matchGivenIndexNotIncludeMatch(barcode, start){
-    let input = `^.{${start}}(.+)` //^.{start}(.+)
-    let index = 1;
-    return this.regEx(input, barcode, index);  
-  }
+  // Case #8
+  // Match on given index, exclude match.
+  matchGivenIndexNotIncludeMatch(barcode, start){
+    let input = `^.{${start}}(.+)` //^.{start}(.+)
+    let index = 1;
+    return this.regEx(input, barcode, index);
+  }
 
 // Case #9
 // Match on given index and given length, include match.
  matchGivenIndexAndSpecificLengthIncludeMatch(barcode, start, length){
- 	 let input = '^.{' + `${start - 1}` +'}' + '(.{' + `${length + 1}` +'}).*'; //^.{start}(.{length}).*
- 	 let index = 1;
-   return this.regEx(input, barcode, index);
+    let input = '^.{' + `${start - 1}` +'}' + '(.{' + `${length + 1}` +'}).*'; //^.{start}(.{length}).*
+    let index = 1;
+   return this.regEx(input, barcode, index);
 }
 
 // Case #10
 // Match on given index and given length, include match.
  matchGivenIndexAndSpecificLengthNotIncludeMatch(barcode, start, length){
    let input = '^.{' + `${start}` +'}' + '(.{' + `${length}` +'}).*'; //^.{start}(.{length}).*
- 	 let index = 1;
-   return this.regEx(input, barcode, index);
+    let index = 1;
+   return this.regEx(input, barcode, index);
 }
-  //----------------------------------------------------------------------------------------------------------------
 
-  //----------------------------------------------------------------------------------------------------------------
 
-	// Combined Methods
-  
-  // Case # 1 and case # 2 combined method
-  // Must provide a BOOLEAN value set (match_from_index_0) to check if it is matching FROM Index_0
-//   matchStringAndReturnWholeString(barcode, input_string, match_from_index_0) {
-//     let input = match_from_index_0 ? `^${input_string}` : `${input_string}`
-//     let regEx = new RegExp(input)
-//     let barcodeMatch = regEx.test(barcode);
+// Combined Methods
 
-//       if (barcodeMatch) {
-//         return barcode
-//       } else {
-//         return "No Match"
-//       }
-//   }
+// Case # 1 and case # 2 combined method
+// Must provide a BOOLEAN value set (match_from_index_0) to check if it is matching FROM Index_0
 
-//   // Case # 3 and case # 4 combined method
-//   // Must provide a boolean value set (include_match) to check if it is INCLUDING MATCH or NOT
+    matchStringAndReturnWholeString(barcode, input_string, match_from_index_0) {
+      let input = match_from_index_0 ? '^' + `${input_string}` + '(.+)' : `${input_string}` + '(.+)';
+      let index = 0;
 
-//   matchAnyPositionReturnStringProceedingMatchIncludingOrNotIncludingMatch(barcode, input_string, include_match) {
-//     let input = `${input_string}`
-//     let regEx = new RegExp(input)
-//     let barcodeMatch = regEx.exec(barcode);
+      return this.regEx(input, barcode, index);
+    }
 
-//     if (barcodeMatch) {
-//     let output = include_match ? barcode.slice(barcodeMatch.index) : barcode.slice(barcodeMatch.index + input.length);
-//       return output
-//     } else {
-//       return "No Match"
-//     }
-//   }
+// Case # 3 and case # 4 combined method
+// Must provide a boolean value set (include_match) to check if it is INCLUDING MATCH or NOT
 
-//   // Case #5 and case #6 combined method
-//   // Must provide a numeric value set for (length) a specific length they want after the match and a boolean value (include_match) set
-//   // if they want to include or exclude the match in return.
+  matchAnyPositionReturnStringProceedingMatchIncludingOrNotIncludingMatch(barcode, input_string, include_match) {
+    let input = include_match ? `${input_string}` + '(.*)' : `${input_string}` + '(.*)'; //input(.*)
+    let index = include_match ? 0 : 1;
 
-//   anyPositionsMatchReturnStringProceedingbyLengthInculdeOrNotIncludeMatch (barcode, input_string, include_match, length) {
-//     let input = `${input_string}`;
-//     let input_length = length
-//     let regEx = new RegExp(input)
-//     let barcodeMatch = regEx.exec(barcode);
-//     
-//     if (barcodeMatch) {
-//       let match_included = barcode.slice(barcodeMatch.index, barcodeMatch.index + input_length + input.length)
-//       let match_excluded = barcode.slice(barcodeMatch.index + input.length, barcodeMatch.index + input.length + length);
-//       let output = include_match ?  match_included :  match_excluded
-//     
-//       return output
-//     } else {
-//       return "No Match"
-//     }
-//   }
+    return this.regEx(input, barcode, index);
+  }
 
-//   // Case #7 and case #8 combined method
-//   // Must provide a (start_postion) and to include or not include_match
 
-//   matchGivenIndexIncludeOrExcludeMatch(barcode, start_position, include_match) {
-//     let match_included = start_position - 1;
-//     let match_excluded = start_position
-//     let start_index = include_match? match_included : match_excluded
-//     let result = barcode.slice(start_index)
-//     
-//     return result;
-//   }
+// Case #5 and case #6 combined method
+// Must provide a numeric value set for (length) a specific length they want after the match and a boolean value (include_match) set
+// if they want to include or exclude the match in return.
 
-//   // Case #9 and case #10 combined method
-//   // Must provide a starting (start) position, return (length) length after the match and a boolean (include_match)
-//   // to include or exclude match
+  anyPositionsMatchReturnStringProceedingbyLengthInculdeOrNotIncludeMatch (barcode, input_string, include_match, length) {
+    // including match RegEx (input.{length})
+    // excluding match RegEx input(.{length})
+    let input = include_match? '(' + `${input_string}` + '.' + '{' + `${length}` +'})' : `${input_string}`+ '(.' + '{' + `${length}` +'})';
+    let index = include_match? 0 : 1
 
-//     matchGivenIndexAndSpecificLengthIncludeOrExcludeMatch(barcode, start, length, include_match) {
-//       let start_index = include_match ? start - 1 : start
-//       let input_length = include_match ? length + 1 : length
-//       let result = barcode.slice(start_index, start_index + input_length);
-//       return result;
-//     }
-//   }
+    return this.regEx(input, barcode, index);
+  }
+
+// Case #7 and case #8 combined method
+// Must provide a (start_postion) and to include or not include_match
+
+  matchGivenIndexIncludeOrExcludeMatch(barcode, start_position, include_match) {
+    // including match RegEx ^.{start - 1}(.+)
+    // excluding match RegEx ^.{start}(.+)
+    let input = include_match ? `^.{${start_position - 1}}(.+)` : `^.{${start_position}}(.+)`
+    let index = 1
+
+    return this.regEx(input, barcode, index);
+  }
+
+//  Case #9 and case #10 combined method
+//  Must provide a starting (start) position, return (length) length after the match and a boolean (include_match)
+//  to include or exclude match
+
+    matchGivenIndexAndSpecificLengthIncludeOrExcludeMatch(barcode, start, length, include_match) {
+      let input = include_match ? '^.{' + `${start - 1}` +'}' + '(.{' + `${length + 1}` +'}).*' : '^.{' + `${start}` +'}' + '(.{' + `${length}` +'}).*'
+      let index = 1
+
+      return this.regEx(input, barcode, index);
+    }
+
+private regEx(input, barcode, index){
+    let regEx = new RegExp(input);
+    let match = regEx.exec(barcode);
+
+     if (match) {
+      return match[index]
+     } else {
+       return null
+     }
+  }
 }
