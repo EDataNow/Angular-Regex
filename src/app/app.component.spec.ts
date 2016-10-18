61156533a93676b490664b0f27bb6977942306ec
naemcivic <naemcivic@gmail.com>
2016-10-18 14:37:45 -0400

    update test cases for new ui methods

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,81 +10,45 @@ describe('AppComponent: AngularRegex', () => {
     app = new AppComponent();
   })
 
+    describe('Match on First Match of', ()=> {
+      describe('Return from position', ()=> {
+        it('#matchOnAnyPositionAndInput should match on any position and return a specfied length string by given position after the match', ()=> {
+          expect(app.matchOnAnyPositionAndInput(barcode, 2, 'A', 2)).toEqual('00');
+        });
 
-  describe('matching string from the whole barcode', ()=> {
+        it('#matchOnAnyPositionAndInput without a specfic length input should return the whole string after the given index', ()=> {
+          expect(app.matchOnAnyPositionAndInput(barcode, 2, 'A', 0)).toEqual('00-H1');
+        });      
+      });
+      
+      describe('Return from match', ()=> {
+        it('#matchOnAnyPositionAndInputProceedingFromMatch should match on any position and return a specfied length string', ()=> {
+          expect(app.matchOnAnyPositionAndInputProceedingFromMatch(barcode, 'A', 2)).toEqual('50');
+        });
+        it('#matchOnAnyPositionAndInputProceedingFromMatch should match on any position and return the complete string if no specific length of return is provided', ()=> {
+          expect(app.matchOnAnyPositionAndInputProceedingFromMatch(barcode, 'A', 0)).toEqual('500-H1');
+        });        
+      });
 
-    it('#matchOnAnyPosition should match whole string character', ()=> {
-      expect(app.matchOnAnyPosition(barcode, '1')).toEqual('P12345-A500-H1');
+      describe('Return from match including the match', ()=> {
+        it('#matchOnAnyPositionIncludeMatchProceed should match on any position and return a specfied length string', ()=> {
+          expect(app.matchOnAnyPositionIncludeMatchProceed(barcode, 'A', 2)).toEqual('A5');
+        });
+        it('#matchOnAnyPositionIncludeMatchProceed should match on any position and return the complete string if no specific length of return is provided', ()=> {
+          expect(app.matchOnAnyPositionIncludeMatchProceed(barcode, 'A', 0)).toEqual('A500-H1');
+        });
+       });
+
+      describe('Returns No Match when', ()=> {
+        it('should return No Match if given return position is greater than the remaining length of the match' , ()=> {
+          expect(app.matchOnAnyPositionAndInput(barcode, 2, 'A', 10)).toEqual("No Match");
+        });
+        it('should return No Match if given return length is greater than the remaining length of the match' , ()=> {
+          expect(app.matchOnAnyPositionAndInput(barcode, 2, 'A', 10)).toEqual("No Match");
+        });
+        it('should return No Match if given return length is greater than the remaining length of the match' , ()=> {
+          expect(app.matchOnAnyPositionIncludeMatchProceed(barcode,'A', 10)).toEqual("No Match");
+        });        
+      });
     });
-
-    it('Should return null when no match is found for #matchOnAnyPosition and #matchOnFirstCharacter, ', ()=> {
-      expect(app.matchOnAnyPosition(barcode, 'X')).toBe(null);
-    });
-  })
-
-  describe('Matching after given string', ()=> {
-
-    it('#matchOnAnyPositionReturnStringProceedingMatchIncludingMatch should match after given string, including match', ()=> {
-      expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode, 'A5')).toEqual('A500-H1');
-    });
-
-    it('#matchOnAnyPositionReturnStringProceedingMatchIncludingMatch should only return matching string, if said string is at the end of barcode', ()=> {
-      expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatch(barcode, 'H1')).toEqual('H1');
-    });
-
-    it('#matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch should match after given string, not including match', ()=> {
-      expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode, 'A')).toEqual('500-H1');
-    });
-
-    it('#matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch should return empty if matching string is at the end of the barcode', ()=> {
-      expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatch(barcode, 'H1')).toEqual('');
-    });
-
-    it('#matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength should return matching string of given length, including match', ()=> {
-      expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode, 'A5', 2)).toEqual('A500');
-    });
-
-    it('#matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength should return matching string of given length, excluding match', ()=> {
-      expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, '0', 3)).toEqual('0-H');
-    });
-
-    it('should return null if given string is at the end of the barcode', ()=> {
-      expect(app.matchOnAnyPositionReturnStringProceedingMatchIncludingMatchOfSpecificLength(barcode, 'H1', 2)).toEqual(null);
-      expect(app.matchOnAnyPositionReturnStringProceedingMatchNotIncludingMatchOfSpecificLength(barcode, 'H1', 2)).toEqual(null);
-    });
-  })
-
-  describe('Matching string on given index and input', ()=> {
-    it('#matchGivenIndexAndInputAndSpecificLengthIncludeMatch should match at given index and input string and length, including match', ()=> {
-      expect(app.matchGivenIndexAndInputAndSpecificLengthIncludeMatch(barcode1, 'A', 2, 3)).toEqual('A345');
-    });
-    it('#matchGivenIndexandInputAndSpecificLengthNotIncludeMatch should match  at given index and input string and length, excluding match', ()=> {
-      expect(app.matchGivenIndexandInputAndSpecificLengthNotIncludeMatch(barcode1, 'A', 2, 3)).toEqual('345');
-    });
-
-    it('#matchGivenIndexAndSpecificLengthIncludeMatch should match after given index and specific length, including match', ()=> {
-      expect(app.matchGivenIndexAndSpecificLengthIncludeMatch(barcode, 8, 3)).toEqual('A500');
-    });
-    it('#matchGivenIndexAndSpecificLengthNotIncludeMatch should match after given index and specific length, excluding match', ()=> {
-      expect(app.matchGivenIndexAndSpecificLengthNotIncludeMatch(barcode, 8, 3)).toEqual('500');
-    });
-
-    it('#matchGivenIndexAndInputIncludingMatchReturnProceedingString should match at given index and input string, including match', ()=> {
-      expect(app. matchGivenIndexAndInputIncludingMatchReturnProceedingString(barcode, 4, '3')).toEqual('345-A500-H1');
-     });
-    it('#matchGivenIndexAndInputIncludingMatchReturnProceedingString should match at given index and input string, including match', ()=> {
-      expect(app. matchGivenIndexAndInputNotIncludingMatchReturnProceedingString(barcode, 4, '3')).toEqual('45-A500-H1');
-    });
-    it('should return null if input is not at given index', ()=> {
-      expect(app.matchGivenIndexAndInputAndSpecificLengthIncludeMatch(barcode1, 'A', 3, 3)).toEqual(null);
-      expect(app.matchGivenIndexandInputAndSpecificLengthNotIncludeMatch(barcode1, 'A', 4, 3)).toEqual(null);
-    });
-
-    it('should return null if given length is greater than the remaining string', ()=> {
-      expect(app.matchGivenIndexAndInputAndSpecificLengthIncludeMatch(barcode1, 'A', 3, 20)).toEqual(null);
-    });
-
-  });
-});
-
-
+  }); 
