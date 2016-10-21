e333b22b11db816627b61bda0b09687c3dc63009
naemcivic <naemcivic@gmail.com>
2016-10-21 11:38:18 -0400

    complete case# 7 - 12 spec tests

--- RegexiOS/RegExProcessor.swift
+++ RegexiOS/RegExProcessor.swift
@@ -21,19 +21,20 @@ class RegExProcessor: NSObject {
     }
     
     // Case # 7 & # 8
-    func matchOnAnyPositionAndInput(input_string:String, specific_length:Int, select_index:Int) -> String  {
+    func matchOnAnyPositionAndInput(input_string:String, specific_length:Int, select_index:Int) -> [String]  {
         let check_specific_length = specific_length > 0
         let sanitizedInput = NSRegularExpression.escapedPattern(for: input_string)
         
-        let regex_input = check_specific_length ? "\(sanitizedInput)(.{\(specific_length)})" : "\(sanitizedInput)(.+)"
+        let regex_input = check_specific_length ? "\(sanitizedInput).{\(select_index - 1 )}(.{\(specific_length)})" : "\(sanitizedInput).{\(select_index - 1 )}(.+)"
         let index = 1
         let collected_matches = regexMatches(pattern: regex_input, in: plainBarcode , index: index)
         
-        return collected_matches[0]
+        return collected_matches
     }
+
     
     // Case # 9 & # 10
-    func matchOnAnyPositionAndInputProceedingFromMatch(input_string:String, specific_length:Int) -> String  {
+    func matchOnAnyPositionAndInputProceedingFromMatch(input_string:String, specific_length:Int) -> [String]  {
         let check_specific_length = specific_length > 0
         let sanitizedInput = NSRegularExpression.escapedPattern(for: input_string)
         
@@ -41,11 +42,11 @@ class RegExProcessor: NSObject {
         let index = 1
         let collected_matches = regexMatches(pattern: regex_input, in: plainBarcode , index: index)
         
-        return collected_matches[0]
+        return collected_matches
     }
     
     // Case # 11 & # 12
-    func matchOnAnyPositionIncludeMatchProceed(input_string:String, specific_length:Int) -> String  {
+    func matchOnAnyPositionIncludeMatchProceed(input_string:String, specific_length:Int) -> [String]  {
         let check_specific_length = specific_length > 0
         let sanitizedInput = NSRegularExpression.escapedPattern(for: input_string)
         
@@ -53,7 +54,7 @@ class RegExProcessor: NSObject {
         let index = 1
         let collected_matches = regexMatches(pattern: regex_input, in: plainBarcode , index: index)
         
-        return collected_matches[0]
+        return collected_matches
     }
     
     
--- RegexiOS/RegexiOSTests/RegExProcessorSpec.swift
+++ RegexiOS/RegexiOSTests/RegExProcessorSpec.swift
@@ -15,25 +15,78 @@ class RegExProcessorSpec: QuickSpec {
     
     override func spec(){
         
-        describe("Quick + Nimble are properly initialized"){
-            
-            describe("A Simple Truth Test"){
-                
-                it("Should expect true to be true"){
-                    expect(true).to(beTrue())
-                }
-                
-            }
-            
-            describe("A Simple Fallacy Test"){
-                
-                it("Should expect false to be false"){
-                     expect(false).to(beFalse())
-                }
-                
-            }
-            
-        }
+        let barcode = "P12345-A500-H1"
+        let regEx = RegExProcessor(barcodeString: barcode )
         
+            describe("Match on First Match of"){
+                describe("#matchOnAnyPositionAndInput should match on any position"){
+                    
+                    context("when given a specific length and given index to return") {
+                        it("should return a specfied length string by given position after the match") {
+                            expect(regEx.matchOnAnyPositionAndInput(input_string: "A", specific_length: 2, select_index: 2)).to(equal(["00"]))
+                        
+                    }
+                    
+                    context("when only given a specifc length") {
+                        it("return the whole string after the given index") {
+                            expect(regEx.matchOnAnyPositionAndInput(input_string: "A", specific_length: 0, select_index: 2)).to(equal(["00-H1"]))
+                        }
+                    }
+                    
+                    context("when given return length is greater than the remainder length of the match") {
+                        it("should return No Match") {
+                            expect(regEx.matchOnAnyPositionAndInput(input_string: "A", specific_length: 10, select_index: 2)).to(equal([]))
+                        }
+                    }
+                    context("when given select index is greater than the remainder length of the match") {
+                        it("should return No Match") {
+                            expect(regEx.matchOnAnyPositionAndInput(input_string: "A", specific_length: 2, select_index: 10)).to(equal([]))
+                        }
+                    }
+                }
+                
+                describe("Return from match"){
+                    describe("#matchOnAnyPositionAndInputProceedingFromMatch should match on any position"){
+                        
+                        context("providing a specific length") {
+                            it("should return the specified length from match") {
+                                expect(regEx.matchOnAnyPositionAndInputProceedingFromMatch(input_string: "A", specific_length: 2)).to(equal(["50"]))
+                            }
+                        }
+                        context("when no specific length is given") {
+                            it("should return everything after the match") {
+                                expect(regEx.matchOnAnyPositionAndInputProceedingFromMatch(input_string: "A", specific_length: 0)).to(equal(["500-H1"]))
+                            }
+                        }
+                        context("when given select index is greater than the remainder length of the match") {
+                            it("should return No Match") {
+                                expect(regEx.matchOnAnyPositionAndInputProceedingFromMatch(input_string: "A", specific_length: 10)).to(equal([]))
+                            }
+                        }
+                    }
+                }
+                
+                describe("Return from match including the match") {
+                    describe("#matchOnAnyPositionIncludeMatchProceed should match on any position"){
+                        
+                        context("providing a specific length") {
+                            it("should return the specified length from match including the match") {
+                                expect(regEx.matchOnAnyPositionIncludeMatchProceed(input_string: "A", specific_length: 2)).to(equal(["A5"]))
+                            }
+                        }
+                        context("when no specific length is given") {
+                            it("should return everything from match including the match") {
+                                expect(regEx.matchOnAnyPositionIncludeMatchProceed(input_string: "A", specific_length: 0)).to(equal(["A500-H1"]))
+                            }
+                        }
+                        context("when given return length is greater than the remainder length of the match") {
+                            it("should return no match") {
+                                expect(regEx.matchOnAnyPositionIncludeMatchProceed(input_string: "A", specific_length: 10)).to(equal([]))
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
