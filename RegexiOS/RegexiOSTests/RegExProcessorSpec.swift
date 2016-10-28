8e1a9ea524c5914d48b818aee7681a5726a0b231
Saim Zahid <saimzahid@me.com>
2016-10-28 13:36:58 -0400

    Regex Swift side complete

--- RegexiOS/RegexiOSTests/RegExProcessorSpec.swift
+++ RegexiOS/RegexiOSTests/RegExProcessorSpec.swift
@@ -19,30 +19,30 @@ class RegExProcessorSpec: QuickSpec {
             describe("Return from postion"){
                 context("it should should return all characters proceeding from given postion"){
                     it("if match is found and length is not specified"){
-                        expect(regex.regexMatches(for: "^.{1}12.{0}(.+)", in: regex.plainBarcode)).to(equal(["345-A500-H?1"]))
+                        expect(regex.regexMatches(for: "^.{1}12.{0}(.+)", in: regex.plainBarcode)).to(equal("345-A500-H?1"))
                     }
                     it("if match is found and length and input_string is not specified"){
-                          expect(regex.regexMatches(for: "^.{1}.{2}(.+)", in: regex.plainBarcode)).to(equal(["345-A500-H?1"]))
+                          expect(regex.regexMatches(for: "^.{1}.{2}(.+)", in: regex.plainBarcode)).to(equal("345-A500-H?1"))
                     }
                     it("even with special characters"){
-                       expect(regex.regexMatches(for: "^.{13}\\?(.+)", in: regex.plainBarcode)).to(equal(["1"]))
+                       expect(regex.regexMatches(for: "^.{13}\\?(.+)", in: regex.plainBarcode)).to(equal("1"))
                     }
                 }
                 context("should return the amount of characters specified by length param proceeding from given postion"){
                     it("if match is found"){
-                         expect(regex.regexMatches(for: "^.{3}3{1}(.{3})", in: regex.plainBarcode)).to(equal(["45-"]))
+                         expect(regex.regexMatches(for: "^.{3}3{1}(.{3})", in: regex.plainBarcode)).to(equal("45-"))
                     }
                     it("if match is found and input_string is not specified"){
-                         expect(regex.regexMatches(for: "^.{3}.{1}(.{3})", in: regex.plainBarcode)).to(equal(["45-"]))
+                         expect(regex.regexMatches(for: "^.{3}.{1}(.{3})", in: regex.plainBarcode)).to(equal("45-"))
                     }
 
                 }
                 context("should return empty"){
                     it("if match is not found and length is not specified"){
-                         expect(regex.regexMatches(for: "^.{1}4.{4}(.+)", in: regex.plainBarcode)).to(equal([]))
+                         expect(regex.regexMatches(for: "^.{1}4.{4}(.+)", in: regex.plainBarcode)).to(equal(""))
                     }
                     it("if match is not found and length is specified"){
-                        expect(regex.regexMatches(for: "^.{1}4.{4}(.{2})", in: regex.plainBarcode)).to(equal([]))
+                        expect(regex.regexMatches(for: "^.{1}4.{4}(.{2})", in: regex.plainBarcode)).to(equal(""))
                     }
                 }
             }
@@ -51,26 +51,26 @@ class RegExProcessorSpec: QuickSpec {
             describe("Return from match"){
                 context("it should return all characters proceeding from match, exlcuding match"){
                     it("if match is found and length is not specified"){
-                         expect(regex.regexMatches(for: "^.{3}3(.+)", in: regex.plainBarcode)).to(equal(["45-A500-H?1"]))
+                         expect(regex.regexMatches(for: "^.{3}3(.+)", in: regex.plainBarcode)).to(equal("45-A500-H?1"))
                                            }
                     it("if match is found and length and input_string is not specified"){
-                         expect(regex.regexMatches(for: "^.{3}.(.+)", in: regex.plainBarcode)).to(equal(["45-A500-H?1"]))
+                         expect(regex.regexMatches(for: "^.{3}.(.+)", in: regex.plainBarcode)).to(equal("45-A500-H?1"))
                     }
                 }
                 context("should return the amount of characters specified by length param proceeding from match, excluding match"){
                     it("if match is found"){
-                        expect(regex.regexMatches(for: "^.{3}3(.{4})", in: regex.plainBarcode)).to(equal(["45-A"]))
+                        expect(regex.regexMatches(for: "^.{3}3(.{4})", in: regex.plainBarcode)).to(equal("45-A"))
                     }
                     it("if match is found and input_string is not specified"){
-                        expect(regex.regexMatches(for: "^.{3}.(.{4})", in: regex.plainBarcode)).to(equal(["45-A"]))
+                        expect(regex.regexMatches(for: "^.{3}.(.{4})", in: regex.plainBarcode)).to(equal("45-A"))
                     }
                 }
                 context("should return empty"){
                     it("if match is not found and length is not specified"){
-                        expect(regex.regexMatches(for: "^.{3}5(.+)", in: regex.plainBarcode)).to(equal([]))
+                        expect(regex.regexMatches(for: "^.{3}5(.+)", in: regex.plainBarcode)).to(equal(""))
                     }
                     it("if match is not found and length is specified"){
-                        expect(regex.regexMatches(for: "^.{3}5.(.{4})", in: regex.plainBarcode)).to(equal([]))
+                        expect(regex.regexMatches(for: "^.{3}5.(.{4})", in: regex.plainBarcode)).to(equal(""))
                     }
                 }
             }
@@ -78,26 +78,26 @@ class RegExProcessorSpec: QuickSpec {
             describe("Return from match(include match)"){
                 context("should return all characters proceeding from match, including match"){
                     it("if match is found and length is not specified"){
-                        expect(regex.regexMatches(for: "^.{1}(1.+)", in: regex.plainBarcode)).to(equal(["12345-A500-H?1"]))
+                        expect(regex.regexMatches(for: "^.{1}(1.+)", in: regex.plainBarcode)).to(equal("12345-A500-H?1"))
                     }
                     it("if match is found and length and input_string is not specified"){
-                       expect(regex.regexMatches(for: "^.{1}(.+)", in: regex.plainBarcode)).to(equal(["12345-A500-H?1"]))
+                       expect(regex.regexMatches(for: "^.{1}(.+)", in: regex.plainBarcode)).to(equal("12345-A500-H?1"))
                     }
                 }
                 context("should return the amount of characters specified by length param proceeding from match, including match"){
                     it("if match is found"){
-                        expect(regex.regexMatches(for: "^.{1}(1.{4})", in: regex.plainBarcode)).to(equal(["12345"]))
+                        expect(regex.regexMatches(for: "^.{1}(1.{4})", in: regex.plainBarcode)).to(equal("12345"))
                     }
                     it("if match is found and input_string is not specified"){
-                        expect(regex.regexMatches(for: "^.{1}(.{5})", in: regex.plainBarcode)).to(equal(["12345"]))
+                        expect(regex.regexMatches(for: "^.{1}(.{5})", in: regex.plainBarcode)).to(equal("12345"))
                     }
                 }
                 context("should return empty"){
                     it("if match is not found and length is not specified"){
-                        expect(regex.regexMatches(for: "^.{1}(6.+)", in: regex.plainBarcode)).to(equal([]))
+                        expect(regex.regexMatches(for: "^.{1}(6.+)", in: regex.plainBarcode)).to(equal(""))
                     }
                     it("if match is not found and length is specified"){
-                        expect(regex.regexMatches(for: "^.{1}(6.{5})", in: regex.plainBarcode)).to(equal([]))
+                        expect(regex.regexMatches(for: "^.{1}(6.{5})", in: regex.plainBarcode)).to(equal(""))
                     }
                 }
             }
@@ -107,23 +107,23 @@ class RegExProcessorSpec: QuickSpec {
             describe("#matchOnFirstMatchReturnFromPosition should match on any position"){
                 context("when given a specific length and given index to return") {
                     it("should return a specfied length string by given position after the match") {
-                        expect(regex.regexMatches(for: "1.{3}(.{5})", in: regex.plainBarcode)).to(equal(["5-A50"]))
+                        expect(regex.regexMatches(for: "1.{3}(.{5})", in: regex.plainBarcode)).to(equal("5-A50"))
                     }
                 }
                 context("when specific length is not specified") {
                     it("return the whole string after the given index") {
-                        expect(regex.regexMatches(for: "1.{3}(.+)", in: regex.plainBarcode)).to(equal(["5-A500-H?1"]))
+                        expect(regex.regexMatches(for: "1.{3}(.+)", in: regex.plainBarcode)).to(equal("5-A500-H?1"))
                     }
                 }
 //
                 context("when given return length is greater than the remainder length of the match") {
                     it("should return No Match") {
-                         expect(regex.regexMatches(for: "1.{3}(.{20})", in: regex.plainBarcode)).to(equal([]))
+                         expect(regex.regexMatches(for: "1.{3}(.{20})", in: regex.plainBarcode)).to(equal(""))
                     }
                 }
                 context("when given select index is greater than the remainder length of the match") {
                     it("should return No Match") {
-                         expect(regex.regexMatches(for: "1.{15}(.+)", in: regex.plainBarcode)).to(equal([]))
+                         expect(regex.regexMatches(for: "1.{15}(.+)", in: regex.plainBarcode)).to(equal(""))
                     }
                 }
             }
@@ -131,17 +131,17 @@ class RegExProcessorSpec: QuickSpec {
                 describe("#matchOnFirstMatchReturnFromMatch should match on any position"){
                     context("providing a specific length") {
                         it("should return the specified length from match") {
-                             expect(regex.regexMatches(for: "1(.{3})", in: regex.plainBarcode)).to(equal(["234"]))
+                             expect(regex.regexMatches(for: "1(.{3})", in: regex.plainBarcode)).to(equal("234"))
                         }
                     }
                     context("when no specific length is given") {
                         it("should return everything after the match") {
-                             expect(regex.regexMatches(for: "1(.+)", in: regex.plainBarcode)).to(equal(["2345-A500-H?1"]))
+                             expect(regex.regexMatches(for: "1(.+)", in: regex.plainBarcode)).to(equal("2345-A500-H?1"))
                         }
                     }
                     context("when given return length is greater than the remainder length of the match") {
                         it("should return No Match") {
-                             expect(regex.regexMatches(for: "1(.{20})", in: regex.plainBarcode)).to(equal([]))
+                             expect(regex.regexMatches(for: "1(.{20})", in: regex.plainBarcode)).to(equal(""))
                         }
                     }
                 }
@@ -152,17 +152,17 @@ class RegExProcessorSpec: QuickSpec {
                     
                     context("providing a specific length") {
                         it("should return the specified length from match including the match") {
-                             expect(regex.regexMatches(for: "(1.{4})", in: regex.plainBarcode)).to(equal(["12345"]))
+                             expect(regex.regexMatches(for: "(1.{4})", in: regex.plainBarcode)).to(equal("12345"))
                         }
                     }
                     context("when no specific length is given") {
                         it("should return everything from match including the match") {
-                            expect(regex.regexMatches(for: "(1.+)", in: regex.plainBarcode)).to(equal(["12345-A500-H?1"]))
+                            expect(regex.regexMatches(for: "(1.+)", in: regex.plainBarcode)).to(equal("12345-A500-H?1"))
                         }
                     }
                     context("when given return length is greater than the remainder length of the match") {
                         it("should return no match") {
-                             expect(regex.regexMatches(for: "(1.{20})", in: regex.plainBarcode)).to(equal([]))
+                             expect(regex.regexMatches(for: "(1.{20})", in: regex.plainBarcode)).to(equal(""))
                         }
                     }
                 }
---
+++ RegexpCommandLine/RegexpCommandLine/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  RegexpCommandLine
+//
+//  Created by Saim Zahid on 2016-10-28.
+//  Copyright © 2016 Saim Zahid. All rights reserved.
+//
+
+import Foundation
+
+let regexCL = RegExProcessor(barcodeString: "123456")
+regexCL.regexMatcher()
+
